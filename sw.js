e7ac3a6a8f2dda98887b141de051f3404d6f53f4
Murillo Yamamoto <mu.yavi@gmail.com>
2023-06-27 17:07:31 -0300

    Att
    testando

--- sw.js
+++ sw.js
@@ -23,11 +23,12 @@ self.addEventListener('fetch', async e => {
     const req = e.request
     const url = new URL(req.url)
 
-    if(url.login === location.origin){
-        e.respondWith(cacheFirst(req))
-    } else{
-        e.respondWith(networkAndCache(req))
+    if (url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+    } else {
+        e.respondWith(networkAndCache(req));
     }
+    
 
 })
 
